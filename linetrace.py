bb8351e52cb63d0aba739c164563a8a04d52c10c
Carpediem324 <101110020+Carpediem324@users.noreply.github.com>
2023-06-15 18:56:56 +0900

    linetrace.py
    linetrace.py

---
+++ linetrace.py
@@ -0,0 +1,75 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+import cv2
+import numpy as np
+from cv_bridge import CvBridge
+
+class LineFollower(Node):
+    def __init__(self):
+        super().__init__('line_follower')
+
+        self.cv_bridge = CvBridge()
+
+        self.image_subscriber = self.create_subscription(
+            Image,
+            '/nanosaur/camera/image_raw',
+            self.image_callback,
+            10)
+        self.cmd_vel_publisher = self.create_publisher(
+            Twist,
+            '/nanosaur/cmd_vel',
+            10)
+
+    def image_callback(self, msg):
+
+        try:
+            image = self.cv_bridge.imgmsg_to_cv2(msg, 'bgr8')
+        except Exception as e:
+            self.get_logger().error('Failed to convert imgmsg to cv2: ' + str(e))
+            return
+        
+        height, width, _ = image.shape
+        roi = image[int(height/2):, :]
+        gray_image = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        _, threshold = cv2.threshold(gray_image, 60, 255, cv2.THRESH_BINARY_INV)
+        contours, _ = cv2.findContours(
+            threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) > 0:
+            largest_contour = max(contours, key=cv2.contourArea)
+            moments = cv2.moments(largest_contour)
+            cX = int(moments["m10"] / moments["m00"])
+            error = cX - int(width/2)
+
+            twist_msg = Twist()
+            twist_msg.angular.z = -float(error) * 0.005
+            twist_msg.linear.x = 0.1
+            self.cmd_vel_publisher.publish(twist_msg)
+        else:
+            self.stop_moving()
+
+    def stop_moving(self):
+        twist_msg = Twist()
+        twist_msg.angular.z = 0.0
+        twist_msg.linear.x = 0.0
+        self.cmd_vel_publisher.publish(twist_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    try:
+        line_follower = LineFollower()
+        rclpy.spin(line_follower)
+
+    except KeyboardInterrupt:
+        pass
+
+    line_follower.stop_moving()
+    line_follower.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
