a7953860ffac4cb2dd70b8d7fd5971478cb5e8e8
TopGun1405 <91329799+TopGun1405@users.noreply.github.com>
2023-06-19 05:21:30 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -105,7 +105,6 @@ class LineFollower(Node):
 
 def find_center(bin_image):
     contours, _ = cv2.findContours(bin_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(out, contours, -1, (255, 255, 0), 1)
 
     center_points = []
     for contour in contours:
