082faf433f32cb6de928d7f5e797cad914fc07b3
namkyu-99 <128462226+namkyu-99@users.noreply.github.com>
2023-06-19 02:27:00 +0900

    Update test_kyu.py

--- test_kyu.py
+++ test_kyu.py
@@ -1,10 +1,10 @@
 import cv2
 import rclpy
 import time
+import numpy as np
 from rclpy.node import Node
 from sensor_msgs.msg import CameraInfo
 from geometry_msgs.msg import Twist
-import numpy as np
 
 def gstreamer_pipeline(capture_width=640, capture_height=480, display_width=640, display_height=480, framerate=30, flip_method=0):
     return (
@@ -29,7 +29,7 @@ def gstreamer_pipeline(capture_width=640, capture_height=480, display_width=640,
 def find_black_regions(img):
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower_black = np.array([0, 0, 0])
-    upper_black = np.array([180, 255, 100]) # Increase the value range to include gray tones
+    upper_black = np.array([180, 255, 70]) # Changing the max black level to better detect lines
     mask = cv2.inRange(hsv_img, lower_black, upper_black)
     return mask
 
@@ -38,7 +38,7 @@ class LineFollower(Node):
         super().__init__('line_follower')
         self.cmd_vel_publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
         self.cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
-        self.timer_period = 0.01
+        self.timer_period = 0.01  # Refresh interval in seconds
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
         self.last_twist = None
 
@@ -56,29 +56,21 @@ class LineFollower(Node):
 
         twist = Twist()
 
-        left_value = center_left.sum()
-        right_value = center_right.sum()
+        linear_speed = 0.2  # Adjust this value to change the robot's speed
 
-        if center_bottom.sum() > left_value and center_bottom.sum() > right_value:
-            # Move straight when the center-bottom is black
-            twist.linear.x = 0.5
+        if (center_bottom.sum() > center_left.sum()) and (center_bottom.sum() > center_right.sum()):
+            # Move straight when center-bottom is black
+            twist.linear.x = linear_speed
             twist.angular.z = 0.0
-        elif left_value > center_bottom.sum() and left_value > right_value:
-            # Turn left when the center-left is black
-            # Update the angular velocity based on the difference between right and left values
-            twist.linear.x = 0.25
-            twist.angular.z = min((left_value - right_value) / 10000, 0.5) # 수정된 부분
-        elif right_value > center_bottom.sum() and right_value > left_value:
-            # Turn right when the center-right is black
-            # Update the angular velocity based on the difference between left and right values
-            twist.linear.x = 0.25
-            twist.angular.z = -min((right_value - left_value) / 10000, 0.5) # 수정된 부분
+
         else:
-            # Stop if no black region is dominant
-            twist.linear.x = 0.0
-            twist.angular.z = 0.0
+            turn_amount = (center_left.sum() - center_right.sum()) / height / width
+            twist.linear.x = linear_speed
+            twist.angular.z = np.clip(turn_amount, -1.0, 1.0) * 0.5
 
-        if self.last_twist is None or (twist.linear.x != self.last_twist.linear.x or twist.angular.z != self.last_twist.angular.z):
+        if (self.last_twist is None
+                or twist.linear.x != self.last_twist.linear.x
+                or twist.angular.z != self.last_twist.angular.z):
             self.cmd_vel_publisher_.publish(twist)
             self.last_twist = twist
             time.sleep(0.1)
