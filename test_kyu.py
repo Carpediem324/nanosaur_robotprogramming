e25ed723a4e0147d316d53a7536076f8a1a356b1
namkyu-99 <128462226+namkyu-99@users.noreply.github.com>
2023-06-19 05:17:10 +0900

    Update test_kyu.py

--- test_kyu.py
+++ test_kyu.py
@@ -4,7 +4,6 @@ import time
 import numpy as np
 from rclpy.node import Node
 from .motor import Motor
-import asyncio
 
 def gstreamer_pipeline(capture_width=640, capture_height=480, display_width=640, display_height=480, framerate=30, flip_method=0):
     return (
@@ -48,57 +47,88 @@ class LineFollower(Node):
         self.rpm = 150
         self.mright = Motor(right_id, self.rpm)
         self.mleft = Motor(left_id, self.rpm)
-        
-        self.corner_detected = False
 
     def timer_callback(self):
         ret_val, img = self.cap.read()
         if ret_val:
             linear_velocity, angular_velocity = self.process_image_and_move(img)
             self.control_motors(linear_velocity, angular_velocity)
-            
-    async def rotate_90_degrees(self):
-        # 로봇을 제자리에서 90도 회전하게 하는 코드 (시계방향 회전)
-        rotation_duration = 1.5  # 실행할 회전 시간(초)
-        target_rpm = 100       # 회전 도중 각 바퀴에서 필요한 RPM
-        self.mright.set_speed(-target_rpm)
-        self.mleft.set_speed(target_rpm)
 
-        await asyncio.sleep(rotation_duration)
+    def turn_90_degrees(self, direction):
+        if direction == "left":
+            angular_velocity = 1.0
+        elif direction == "right":
+            angular_velocity = -1.0
+        else:
+            return
 
-        self.mright.set_speed(0)
-        self.mleft.set_speed(0)
-        self.corner_detected = False
+        turning_duration = 1.55  # Adjust this value for a precise 90-degree turn
+        start_time = time.time()
+        while time.time() - start_time < turning_duration:
+            self.control_motors(0, angular_velocity)
+        self.control_motors(0, 0)
 
+    def move_forward(self, duration, speed=0.5):
+        start_time = time.time()
+        while time.time() - start_time < duration:
+            self.control_motors(speed, 0)
+        self.control_motors(0, 0)
+        
     def process_image_and_move(self, img):
-        left_mask, right_mask = self.generate_masks()
-        img_points = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        black_regions = find_black_regions(img)
+        height, width = black_regions.shape
+        left_area = black_regions[int(height * 0.4):int(height * 0.6), :int(width * 1/3)]
+        center_area = black_regions[:, int(width * 1/3):int(width * 2/3)]
+        right_area = black_regions[int(height * 0.4):int(height * 0.6), int(width * 2/3):]
 
-        left_points = cv2.bitwise_and(img_points, left_mask)
-        right_points = cv2.bitwise_and(img_points, right_mask)
+        edge_weight = 15
 
-        left_edge = cv2.Canny(left_points, 100, 120)
-        right_edge = cv2.Canny(right_points, 100, 120)
+        left_edge = black_regions[int(height * 0.4):int(height * 0.6), int(width * 0.1):int(width * 1/3)]
+        right_edge = black_regions[int(height * 0.4):int(height * 0.6), int(width * 2/3):int(width * 0.9)]
 
-        edge_weight = 1.0 / (len(img_points)*len(img_points[0]))
-        threshold = 0.015
+        linear_velocity = 0.0
+        angular_velocity = 0.0
+        
+        # 코너 회전 구간 설정
+        corner_threshold = 10000  # 코너를 감지할 픽셀 개수 임계값 설정
 
-        if not self.corner_detected and right_edge.sum()*edge_weight > threshold and left_edge.sum()*edge_weight > threshold:
-            self.corner_detected = True
-            self.get_logger().info("Corner detected")
-            asyncio.run(self.rotate_90_degrees())
+        # 코너를 감지한 경우
+        if left_edge.sum() > corner_threshold or right_edge.sum() > corner_threshold:
 
-        if self.corner_detected:
+            # 코너 끝까지 직진
+            moving_duration = 1.2  # 코너 끝까지 이동하는데 걸리는 시간
+            self.move_forward(moving_duration)
+
+            # 제자리에서 90도 회전
+            if left_edge.sum() > corner_threshold:
+                self.turn_90_degrees("right")
+            else:
+                self.turn_90_degrees("left")
+
+            # 회전 후 앞으로 진행
+            self.move_forward(moving_duration)
+
+            # 속도와 각속도 재설정
             linear_velocity = 0.0
             angular_velocity = 0.0
+
+        if center_area.sum() > left_area.sum() + left_edge.sum() * edge_weight and center_area.sum() > right_area.sum() + right_edge.sum() * edge_weight:
+            linear_velocity = 0.5
+            angular_velocity = 0.0
+        elif left_area.sum() + left_edge.sum() * edge_weight > center_area.sum() and left_area.sum() + left_edge.sum() * edge_weight > right_area.sum() + right_edge.sum() * edge_weight:
+            z_compensation = (right_area.sum() - left_area.sum()) / (left_area.sum() + right_area.sum())
+            linear_velocity = 0.15
+            angular_velocity = 0.5 + z_compensation * 0.8  # 코너 회전 위치 조정
+        elif right_area.sum() + right_edge.sum() * edge_weight > center_area.sum() and right_area.sum() + right_edge.sum() * edge_weight > left_area.sum() + left_edge.sum() * edge_weight:
+            z_compensation = (left_area.sum() - right_area.sum()) / (left_area.sum() + right_area.sum())
+            linear_velocity = 0.15
+            angular_velocity = -0.5 - z_compensation * 0.8  # 코너 회전 위치 조정
         else:
-            left_intensity = left_edge.sum()*edge_weight
-            right_intensity = right_edge.sum()*edge_weight
+            linear_velocity = 0.0
+            angular_velocity = 0.0
 
-            difference = right_intensity - left_intensity
-
-            linear_velocity = self.base_speed
-            angular_velocity = self.base_speed * difference * self.gain
+        cv2.imshow("Processed Image", black_regions)
+        cv2.waitKey(1)
 
         return linear_velocity, angular_velocity
 
